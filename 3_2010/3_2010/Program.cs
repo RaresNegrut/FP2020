46bd4a85293440e29365a633bb3d83f343ed3520
TheJanitor1151 <45803183+TheJanitor1151@users.noreply.github.com>
2020-10-20 10:07:22 +0300

    wrote a gauss function, but didn't finish the ordering

--- 3_2010/3_2010/Program.cs
+++ 3_2010/3_2010/Program.cs
@@ -4,6 +4,19 @@ namespace _3_2010
 {
     class Program
     {
+        static int Gauss(int n)
+        {
+            int s = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                s += i;
+            }
+            return s;
+        }
+        Ordonare3V2(int a, int b,int c)
+        {
+
+        }
         static void Main(string[] args)
         {
             //programare structurata
@@ -21,11 +34,27 @@ namespace _3_2010
             //organizarea codului sub forma de proceduri/functii(metode)
 
             //gaussian sum
-            int n,s=0;
+            int n;
             Console.WriteLine("enter a number for the Gaussian sum");
             n=int.Parse(Console.ReadLine());
-            s = ((n + 1) * n) / 2;
+            Console.WriteLine(Gauss(n));
+            //s = ((n + 1) * n) / 2;
             Console.Write($"The sum of numbers from 1 to {n} is:{s}");
+            //sb
+            int a, b, c;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            //if(a<b)
+            //{
+            //    if (a < c)
+            //    {
+            //        Console.Write(a);
+
+            //    }
+                
+
+            //}
 
         }
     }
